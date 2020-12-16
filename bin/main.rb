bc4c3771518147e62865e2a08ad9b99775b18f87
helman101 <helmanandresortegon@gmail.com>
2020-12-16 11:08:03 +0100

    add a start method

--- bin/main.rb
+++ bin/main.rb
@@ -1,3 +1,18 @@
 require_relative '../lib/remby_bot'
 
-Bot.new
+require_relative '../lib/message_helper'
+require_relative '../lib/commands/drink_water'
+
+token = '1489570711:AAGhIHN0jjxLXA1yXtlZAPvyc0tYH5LLcCc'
+
+def start_bot(token)
+  Telegram::Bot::Client.run(token) do |bot|
+    bot.listen do |message|
+      new_message = MessageHelper.new(bot, message)
+      new_message.help_message
+    end
+  end
+end
+
+start_bot(token)
+
