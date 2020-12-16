b28554958d7f4208c4c16b88a1785edaa68262f8
helman101 <helmanandresortegon@gmail.com>
2020-12-16 13:02:58 +0100

    create main_spec

--- bin/main.rb
+++ bin/main.rb
@@ -5,13 +5,14 @@ require_relative '../lib/commands/drink_water'
 
 token = '1489570711:AAGhIHN0jjxLXA1yXtlZAPvyc0tYH5LLcCc'
 
-def start_bot(token)
-  Telegram::Bot::Client.run(token) do |bot|
-    bot.listen do |message|
-      new_message = MessageHelper.new(bot, message)
-      new_message.help_message
+class Remby
+  def start_bot(token)
+    Telegram::Bot::Client.run(token) do |bot|
+      bot.listen do |message|
+        new_message = MessageHelper.new(bot, message)
+        new_message.help_message
+      end
     end
   end
 end
-
 start_bot(token)
